b5d42e21b0ffa012c93934385568bba22b7304dc
AswathiMohan23 <achu999here@gmail.com>
2022-12-28 08:02:53 +0530

    [ADDED] : code to validate regex using pojo class

--- js/employeePayrollForm.js
+++ js/employeePayrollForm.js
@@ -5,12 +5,71 @@ salary.addEventListener('input',function(){
     output.textContent=salary.value;
 });  
 
-const text=document.querySelector('#text');
-const textError=document.querySelector('.text-error');
-text.addEventListener('input',function(){
-    let nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if(nameRegex.test(text.value))
-        textError.textContent="";
-    else 
-        textError.textContent="Name is Incorrect"  
-});
+
+class EmployeeData{
+    name;
+    profileImage;
+    gender;
+    department;
+    phone;
+    email;
+    salary;
+    startDate;
+    notes;
+
+    //constructor
+
+    constructor(...params){
+        this.setName=params[0];
+        this.setProfilePic=params[1];
+        this.setGender=params[2];
+        this.setDepartment=params[3];
+        this.setPhone=params[4];
+        this.setEmail=params[5];
+        this.setSalary=params[6];
+        this.setStartDate=params[7];
+        this.setNotes=params[8];
+    }
+    get getName(){
+        return this.name;
+    }
+    get getProfilePic(){
+        return this.profilePic;
+    }
+    get getGender(){
+        return this.gender;
+    }
+    get getDept(){
+        return this.Dept;
+    }
+    get getPhone(){
+        return this.phone;
+    }
+    get getEmail(){
+        return this.email;
+    }
+    get getSalary(){
+        return this.salary;
+    }
+    get getStartDate(){
+        return this.startDate;
+    }
+    get getNotes(){
+        return this.notes;
+    }
+
+    set setName(name){
+        let nameRegex=RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if(nameRegex.test(name))
+            this.name=name;
+        else throw "name is invalid";
+    }
+    
+    set setStartDate(startDate){
+        let startDateRegex=RegExp("^\\d[2010-2022]{4}-\\d[01-12]{2}-\\d[01-31]{2}$");
+        if(startDateRegex.test(startDate))
+            this.startDate=startDate;
+        else throw "start date is invalid"
+    }
+
+}
