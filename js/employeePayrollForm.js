ed3d6c1944168d306449b1c1ce516584c5659c7d
AswathiMohan23 <achu999here@gmail.com>
2022-12-31 16:38:24 +0530

    [ADDED] : code to create employee payroll object on save

--- js/employeePayrollForm.js
+++ js/employeePayrollForm.js
@@ -21,9 +21,55 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     salary.addEventListener('input',function(){
         output.textContent=salary.value;
     });
+
+    const save=()=>{
+        try{
+            let EmployeePayrollData=createEmployeePayroll();
+        }catch(e){
+            return;
+        }
+    }
+
+    const createEmployeePayroll=()=>{
+        let employeePayrollData=new EmployeePayrollData();
+        try{
+            employeePayrollData.name=getInputValueById('#name');
+        }catch(e){
+            setTextValue('.text-error',e);
+            throw e;
+        }
+        employeePayrollData.profilePic=getSelectedValues('[name=profile]').pop();
+        employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+        employeePayrollData.department=getSelectedValues('[name=department]');
+        employeePayrollData.salary=getInputValueById('#salary');
+        employeePayrollData.note=getInputValueById('#notes');
+        let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+        employeePayrollData.date=Date.parse(date);
+        alert(employeePayrollData.toString());
+        return employeePayrollData;
+    }
+
+    const getSelectedValues=(propertyValue) =>{
+        let allItems=document.querySelectorAll(propertyValue);
+        let seLiTEMS=[];
+        allItems.forEach(item=>{
+            if(item.checked)seLiTEMS.push(item.value);
+        });
+        return seLiTEMS;
+    }
+
+    const getInputValueById=(id) =>{
+        let value=document.querySelector(id).value;
+        return value;
+    }
+
+    const getInputElementValue=(id)=>{
+        let value=document.getElementById.value;
+        return value;
+    }
 });  
 
-class EmployeeData{
+class EmployeePayrollData{
     id;
     name;
     profilePic;
