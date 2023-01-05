6fb63156f171f244df1af6522326892b84c95b70
AswathiMohan23 <achu999here@gmail.com>
2023-01-05 09:48:18 +0530

    [ADDED] : code to display employee details from JSON object including department

---
+++ js/homePage.js
@@ -0,0 +1,59 @@
+window.addEventListener('DOMContentLoaded',(event) => {
+    createInnerHtml();
+});
+
+
+// Template literal ES6 feature
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if(employeePayrollList.length==0)return;                    
+    let innerHtml = `${headerHtml}`;
+    let employeePayrollList = createEmployeePayrollJSON();
+    for (const employeePayrollData of employeePayrollList) {
+        innerHtml = `${innerHtml}
+            <tr>
+                <td><img class="profile" src="${employeePayrollData._profilePic}" alt=""></td>
+                <td>${employeePayrollData._name}</td>
+                <td>${employeePayrollData._gender}</td>
+                <td>${getDeptHtml(employeePayrollData._department)}</td>
+                <td>${employeePayrollData._salary}</td>
+                <td>${employeePayrollData._startDate}</td>
+                <td>
+                    <img name="${employeePayrollData._id}" onclick="remove(this)"
+                        src="./assets/icons/delete-black-18dp.svg" alt="delete">
+                    <img name="${employeePayrollData._id}" onclick="update(this)"
+                        src="./assets/icons/create-black-18dp.svg" alt="edit">
+                </td>
+            </tr>
+        `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON=()=>{
+    let empPayrollListLocal=[
+        {
+            _name:'Harry Potter',
+            _gender:'male',
+            _department:[
+                'Engineerig',
+                'Fimamce'
+            ],
+            _salary:'5000000',
+            _startDate:'29 oct 2019',
+            _note:'',
+            _id:new Date().getTime(),
+            _profilePic:'../assets/profile-images/Ellipse-5.png'
+
+        }
+    ];
+    return empPayrollListLocal;
+}
+const getDeptHtml=(deptList) => {
+    let getDeptHtml='';
+    for(const dept of deptList){
+        deptHtml=`${deptHtml} < div class='dept-label'> ${dept}<div>`
+    }
+    return deptHtml;
+}
